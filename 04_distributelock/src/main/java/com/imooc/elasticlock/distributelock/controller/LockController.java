6a191b97b79c013106f9cb359383e168febb57eb
564155611 <564155611@qq.com>
2021-12-12 14:57:55 +0800

    提交

---
+++ 04_distributelock/src/main/java/com/imooc/elasticlock/distributelock/controller/LockController.java
@@ -0,0 +1,8 @@
+package com.imooc.elasticlock.distributelock.controller;
+
+public class LockController {
+
+    public static void main(String[] args) {
+        org.mybatis.generator.config.xml.ConfigurationParser configurationParser;
+    }
+}
